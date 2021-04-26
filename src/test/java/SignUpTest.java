a0f74b6c01eeb0d4e8a154840e577be20f4da15c
Vadim <Vadim8730@gmail.com>
2021-04-26 15:48:14 +0300

    Register and bay book(The Adventures of Huckleberry Finn).

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class SignUpTest {
 
     @Test
@@ -106,7 +108,7 @@ public class SignUpTest {
     }
 
     @Test
-    public void SignUpNameNull() {
+    public void signUpNameNull() {
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         //Ввести любые 5 цифр (12345)
         //Нажать кнопку Continue
@@ -135,7 +137,7 @@ public class SignUpTest {
     }
 
     @Test
-    public void SignUpAllFieldIsNull() {
+    public void signUpAllFieldIsNull() {
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         //Ввести любые 5 цифр (12345)
         //Нажать кнопку Continue
@@ -164,7 +166,7 @@ public class SignUpTest {
     }
 
     @Test
-    public void SignUpAllFirstNameDigits() {
+    public void signUpAllFirstNameDigits() {
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         //Ввести любые 5 цифр (12345)
         //Нажать кнопку Continue
@@ -194,7 +196,7 @@ public class SignUpTest {
     }
 
     @Test
-    public void SignUpPasswordAndConfirmPasswordNotTheSame() {
+    public void signUpPasswordAndConfirmPasswordNotTheSame() {
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         //Ввести любые 6 цифр (123456)
         //Нажать кнопку Continue
@@ -224,7 +226,7 @@ public class SignUpTest {
     }
 
     @Test
-    public void SignUpPasswordNull() {
+    public void signUpPasswordNull() {
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         //Ввести любые 6 цифр (123456)
         //Нажать кнопку Continue
@@ -233,6 +235,7 @@ public class SignUpTest {
         //Установка переменной среды
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
         //Открыть страницу https://sharelane.com/cgi-bin/register.py
         driver.get("https://sharelane.com/cgi-bin/register.py");
         //Ввести любые 5 цифр (12345)
@@ -253,6 +256,80 @@ public class SignUpTest {
 
     }
 
+    @Test
+    public void logInAndPassword() {
+        //Открыть страницу https://sharelane.com/cgi-bin/register.py
+        //Ввести любые 6 цифр (123456)
+        //Нажать кнопку Continue
+        //Убедиться что мы на странице SIGN UP
+        //Закрыть браузер
+        //Установка переменной среды
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+        //Открыть страницу https://sharelane.com/cgi-bin/register.py
+        driver.get("https://sharelane.com/cgi-bin/register.py");
+        //Ввести любые 5 цифр (12345)
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        //Нажать кнопку Continue
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        driver.findElement(By.name("last_name")).sendKeys("Potter");
+        driver.findElement(By.name("email")).sendKeys("alexpotter@hfh.com");
+        driver.findElement(By.name("password1")).sendKeys("12345");
+        driver.findElement(By.name("password2")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+//        String massage = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+//        Assert.assertEquals(massage, "Oops, error on page. Some of your fields have invalid data or email was " +
+//                "previously used", "Удивительно, но работает");
+        String email = driver.findElement(By.xpath("(//b)[2]")).getText();
+        driver.get("https://sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("1111");
+        driver.findElement(By.cssSelector("[value=Login]")).click();
 
+//        //Закрыть браузер
+//        driver.quit();
 
+    }
+
+    @Test
+    public void buyBookAndChangeQuantity() {
+        //Открыть страницу https://sharelane.com/cgi-bin/register.py
+        //Ввести любые 6 цифр (123456)
+        //Нажать кнопку Continue
+        //Убедиться что мы на странице SIGN UP
+        //Закрыть браузер
+        //Установка переменной среды
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+        //Открыть страницу https://sharelane.com/cgi-bin/register.py
+        driver.get("https://sharelane.com/cgi-bin/register.py");
+        //Ввести любые 5 цифр (12345)
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        //Нажать кнопку Continue
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        driver.findElement(By.name("last_name")).sendKeys("Potter");
+        driver.findElement(By.name("email")).sendKeys("alexpotter@hfh.com");
+        driver.findElement(By.name("password1")).sendKeys("12345");
+        driver.findElement(By.name("password2")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String email = driver.findElement(By.xpath("(//b)[2]")).getText();
+        driver.get("https://sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("1111");
+        driver.findElement(By.cssSelector("[value=Login]")).click();
+        driver.findElement(By.name("keyword")).sendKeys("The Adventures of Huckleberry Finn");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.findElement(By.xpath(("//a[contains(@href, 'add_to_cart')]") )).click();
+//        driver.get("https://www.sharelane.com/cgi-bin/shopping_cart.py");
+        driver.findElement(By.xpath(("//a[contains(@href, 'shopping_cart')]") )).click();
+        driver.findElement(By.name("q")).sendKeys("19");
+        driver.findElement(By.cssSelector("[value=Update]")).click();
+        String massage = driver.findElement(By.cssSelector("[class=confirmation_message]")).getText();
+        Assert.assertEquals(massage, "Cart Updated", "ERROR");
+        driver.quit();
+    }
 }
